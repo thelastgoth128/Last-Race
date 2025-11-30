a42ad1049fba696cb624094951ab7931f718fb2a
thelastgoth128 <thindwacyrus86@gmail.com>
2025-11-30 03:20:58 +0200

    fix: rename function parameters for consistency in gameover and pause scripts

--- scripts/gameover.gd
+++ scripts/gameover.gd
@@ -4,6 +4,6 @@ func _ready():
 	visible = false
 	SignalBus.connect("Race_finished", Callable(self, "_show_game_over"))
 	
-func _show_game_over(car: Node):
+func _show_game_over(_car: Node):
 	visible =  true
 	
